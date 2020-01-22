c0b7f423a2d9907a54d44068f1fb6867dca6ea7e
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-22 05:28:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 
-def countdown(seconds)
+def countdown(seconds_left_to_midnight)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
@@ -10,10 +10,10 @@ def countdown(seconds)
 end
 
 
-def countdown_with_sleep(time)
-    while time > 0
-    puts `#{time} SECOND(S)!`
-    time -= 1
+def countdown_with_sleep(time_remaining_until_midnight)
+    while time_remaining_until_midnight > 0
+    puts `#{time_remaining_until_midnight} SECOND(S)!`
+    time_remaining_until_midnight -= 1
     sleep(1)
   end
   
